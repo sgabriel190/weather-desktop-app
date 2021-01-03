a25b5bb69a1904e2fe69380c0f9bf0f50ad84973
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2021-01-03 19:25:45 +0200

    Updated gui.

--- app/main.py
+++ app/main.py
@@ -1,2 +1,27 @@
+import os
+import sys
+
+import qdarkstyle
+from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.uic import loadUi
+
+
+class WeatherApp(QDialog):
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    def __init__(self):
+        super(WeatherApp, self).__init__()
+        ui_path = os.path.join(self.ROOT_DIR, 'weather-app.ui')
+        loadUi(ui_path, self)
+
+
 if __name__ == "__main__":
-    pass
+    app = QApplication(sys.argv)
+
+    stylesheet = qdarkstyle.load_stylesheet_pyqt5()
+    app.setStyleSheet(stylesheet)
+
+    window = WeatherApp()
+    window.show()
+    sys.exit(app.exec_())
+
